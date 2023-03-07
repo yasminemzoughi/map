d6c87eb3af05865b45fa02121ff8148ea92858d4
yasminemzoughi <yasminemzoughi4@gmail.com>
2023-03-07 17:00:12 +0100

    firstcommit

---
+++ src/Components/Home/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./Home.css"
+const Home = () => {
+  return (
+    <div className='map'>
+      
+      <iframe  title='map'src="https://maps.google.com/maps?q=2880%20Broadway,%20New%20York&t=&z=13&ie=UTF8&iwloc=&output=embed"></iframe>
+  </div>
+  )
+}
+
+export default Home
